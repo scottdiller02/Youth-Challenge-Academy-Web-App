af6e696c9a2e30e91910ba846927058693bf7a32
brandonflynt <brandon.flynt@bobcats.gcsu.edu>
2018-04-13 01:36:26 -0400

    view staff records
    view/edit staff for admin, edit is wip

--- routers/getStaffRecords.js
+++ routers/getStaffRecords.js
@@ -4,33 +4,28 @@ var express = require('express')
 var db=require("../db");
  
 router.get("/getStaffRecords", function(req, res){
-	var collection1 = db.getDb().collection('staff')
+	var collection = db.getDb().collection('staff')
 	res.setHeader("Content-Type", "application/json");
-	collection1.find().toArray(function(err, docs){
-	//docs contains all records from phase1 in 
+	collection.find().toArray(function(err, docs){
+	//docs contains all records from cadets in 
 	//js array format
-	var info1=[];
-	for(doc1 of docs1) 
-	info1.push(doc1);
-	res.json(info1);
+	var info=[];
+	for(doc of docs) 
+	info.push(doc);
+	res.json(info);
+	//console.log(info);
+	//res.render('cadets', {info: docs})
+
 	})
-	//collection2.find().toArray(function(err, docs){
-	//docs contains all records from phase2 in 
-	//js array format
-	//var info2=[];
-	//for(doc2 of docs2) 
-	//info2.push(doc2);
-	//res.json(info2);
-	//})
 })
 
 
-router.get("/staff", function(req, res){
-	var collection1 = db.getDb().collection('staff');
+router.get("/staffRecords", function(req, res){
+	var collection = db.getDb().collection('staff');
 
-	collection1.find().toArray(function(err, docs1){
+	collection.find().toArray(function(err, docs){
 		//collection2.find().toArray(function(err, docs2){
-			res.render('staff', {infoP1: docs1, infoP2: docs2})
+			res.render('staffRecords', {infoCadets: docs})
 		})
 	//})
 })
